ad32840288e5e91f1252af449067dba141718e5d
skopje <30603816+skopje@users.noreply.github.com>
2017-08-02 18:25:14 +0300

    15.Ters Kosegen Matris
    Made by Skopje

---
+++ TersKosegen.java
@@ -0,0 +1,17 @@
+public static void main(String[] args) {
+      System.out.print("Lutfen ters kosegen matrisin boyutlarını giriniz: ");
+      Scanner s = new Scanner(System.in);
+      int sayi = s.nextInt();
+      for(int i=1;i<sayi;i++){
+          for(int j=1;j<sayi;j++){
+              if((i+j)==(sayi))
+                  System.out.print(1+" ");
+              else
+                  System.out.print(0+" ");
+          }
+          System.out.println("");
+      }
+        
+        
+    }
+}
